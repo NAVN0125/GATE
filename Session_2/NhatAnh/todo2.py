eb6ffc75dbbf895dad7188fb477e43189fd3d0ee
NAVN0125 <88468395+NAVN0125@users.noreply.github.com>
2022-11-14 18:42:38 +0700

    Add files via upload

---
+++ Session_2/NhatAnh/todo2.py
@@ -0,0 +1,11 @@
+text_type = "string"
+numeric_type = 1
+sequence_type = [1,2,3]
+mapping_type = {
+    a = "a"
+    b = "b"
+}
+set_type = {a,b,c,d}
+boolean_type = true
+binary_type = int(123,2)
+none_type = null
