feab29d63e85d8ebcd9e20a395a5f9422892171b
DuckZ8657 <minhbb.ze@gmail.com>
2022-11-20 21:15:37 +0700

    DucMinh

--- session3/input_char.py
+++ session3/input_char.py
@@ -1,1 +1,13 @@
-print("The most important syntax in Python is indentation, which needs to be at least 1 space, but normally is 4 spaces.")
+lowercase = "abcdefghijklmnopqrstuvwxyz"
+uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+numset = "1234567890"
+nonumber = 0
+nolowercase = 0
+nouppercase = 0
+def getstring():
+    userstrinput = input("Please enter a string, we will count the lower and uppercase letters, including the numbers in your string: ")
+
+def printstring():
+    print("The numbers of lowercase letters are: " + nolowercase)
+    print("The numbers of uppercase letters are: " + nouppercase)
+    print("The numbers of numbers are: " + nonumber)
