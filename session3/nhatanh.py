74571d551f8a63af0f06662a10194de0b7bc011e
NAVN0125 <88468395+NAVN0125@users.noreply.github.com>
2022-11-25 20:36:58 +0700

    Add files via upload

---
+++ session3/nhatanh.py
@@ -0,0 +1,13 @@
+def checknum(userstrinput):
+    for x in userstrinput:
+        for y in range (numset.len()):
+			if numset[y] = userstrinput[x]:
+		    	nonumber += 1
+
+def checkcase(userstrinput):
+    for x in userstrinput:
+        for y in range (lowercase.len()):
+			if lowercase[y] = userstrinput[x]:
+		    	nolowercase += 1
+			elif uppercase[y] = userstrinput[x]:
+		    	nouppercase += 1
